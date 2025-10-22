4458562051f35448e56af0e24108c2f16d97112f
Sergey Malyuga <sergey@malyuga.ru>
2025-10-22 14:22:37 +0300

    Создать main-component, header.component. Создать эффекты при взаимодествии с элементами header.component

---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeaderComponent } from '../../features/header/header.component';
+
+@Component({
+  selector: 'app-main',
+  imports: [HeaderComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MainComponent {
+
+}
