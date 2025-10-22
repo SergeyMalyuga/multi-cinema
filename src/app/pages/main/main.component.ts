0c5b60703bb0e0947a85d0fa6c38396279d891c2
Sergey Malyuga <sergey@malyuga.ru>
2025-10-22 16:13:03 +0300

    Создать hero.component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,13 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../../features/header/header.component';
+import { HeroComponent } from '../../features/hero/hero.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HeroComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
